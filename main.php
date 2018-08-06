b240aceb3cd5dda49f579b5d50b3d9c9e674d88f
Jen Wachter <jenleighkelly@gmail.com>
2018-08-06 09:21:40 -0400

    Updated version number

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Sidebar Picker
 Description: Adds the ability to choose a sidebar item or default to the inherited page's related content item.
 Author: Jen Wachter
-Version: 0.1
+Version: 0.3
 */
 
 // exit if accessed directly
