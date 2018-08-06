b8bde7c2d8b8e08151f5c758a6533a48c5aa624d
Jen Wachter <jenleighkelly@gmail.com>
2018-08-06 09:10:11 -0400

    Auto enabled sidebar picker when the plugin is activated

--- main.php
+++ main.php
@@ -6,16 +6,72 @@ Author: Jen Wachter
 Version: 0.1
 */
 
-class SidebarPicker
-{
-  public function __construct($logger)
-  {
-    $this->logger = $logger;
-
-    add_action('acf/include_field_types', function () {
-      new \SidebarPicker\Field($this->logger);
-    });
-  }
+// exit if accessed directly
+if (!defined('ABSPATH')) {
+  exit;
 }
 
-new SidebarPicker($dependencies["logger_wp"]);
+// check if class already exists
+if (!class_exists('SidebarPickerField')):
+
+class SidebarPickerField extends acf_field
+{
+  public $defaults;
+
+  public function __construct()
+  {
+    $this->name = 'sidebar_picker';
+    $this->label = __('Sidebar Picker');
+    $this->category = __("Choice",'acf');
+    $this->defaults = array(
+      "multiple" =>  0
+    );
+
+    parent::__construct();
+  }
+
+  protected function getSidebarGroups()
+  {
+    $posts = get_posts(array(
+      "post_type" => "sidebar",
+      "posts_per_page" => -1
+    ));
+
+    $sidebar = array();
+
+    foreach ($posts as $post) {
+      $sidebar[$post->ID] = $post->post_title;
+    }
+
+    $sidebar["inherit"] = "Inherit Parent Sidebar";
+    $sidebar[""] = "None";
+
+    return $sidebar;
+  }
+
+  public function render_field($field)
+  {
+    $value = $field["value"] ? $field["value"] : "";
+
+    echo "<div class='acf-input-wrap'>";
+    echo '<select id="' . $field['id'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '" >';
+
+    $sidebars = $this->getSidebarGroups();
+
+    foreach($sidebars as $k => $v) {
+
+      $selected = $k == $value ? "selected='selected'" : "";
+      echo "<option value='{$k}' {$selected}>{$v}</option>";
+    }
+
+    echo "</select>";
+    echo "</div>";
+  }
+
+}
+
+// initialize
+new SidebarPickerField();
+
+// class_exists check
+endif;
